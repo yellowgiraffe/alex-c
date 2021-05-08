5be8945d6b13de3802315ba9698a8f015c92f88f
Olga Mirocz <ol.miroch@gmail.com>
2021-05-08 11:47:18 +0300

    Homework for lecture 2

--- Lecture 2/Main homework/script.js
+++ Lecture 2/Main homework/script.js
@@ -1,3 +1,17 @@
-let number = a;
+let budget = +prompt("Ваш бюджет", "Введите ваш бюджет");
+const storeName = prompt("Название вашего магазина?");
 
-function a() {};
+let myShop = {
+    budget: budget,
+    name: storeName,
+    shopProduct: [],
+    personal: {},
+    open: false,
+}
+
+myShop.shopProduct.push(prompt("Что будем продовать?"));
+myShop.shopProduct.push(prompt("Что будем продовать?"));
+myShop.shopProduct.push(prompt("Что будем продовать?"));
+
+let userAge = +prompt("Сколько вам лет?", "Введите свой возраст");
+alert(typeof userAge === "number" && userAge || "Пользователь ответил неверно");
